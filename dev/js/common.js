e6d07de6953c19a1f4926940c49399055558c893
00King00 <mr.1bilous@gmail.com>
2018-06-12 16:38:08 +0300

    fixed conflict

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,15 +1,15 @@
 $(function(){
 	var review_slider=$('#review-slider').lightSlider({
 		item: 1,
-		controls: true,
+		controls: false,
 		pager: false
 	});
-	var icon_review_slider_prev = $("icon_review_slider_prev");
+	//var icon_review_slider_prev = $("icon_review_slider_prev");
 	
 	function review_slider_pager(){
 		var allNumber = $('#review-slider>li').length;
-		var index = $(".active")
-		var curentIndex =  $('li.lslide').index(index) +1;
+		var index = $("#review-slider>.active");
+		var curentIndex =  $('#review-slider>li').index(index) +1;
 		$(".review-slider__pager span:first-child").text(curentIndex);
 		$(".review-slider__pager span:last-child").text(allNumber);
 		
